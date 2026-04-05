eaaecbb6a6d3af47e848f0a66b1a9e14646a7811
Roberto Lucas <lucascastilloroberto@gmail.com>
2026-04-05 17:14:32 +0200

    Scripts updated

--- Assets/Script/Stickman.cs
+++ Assets/Script/Stickman.cs
@@ -1,6 +1,16 @@
 using UnityEngine;
 
-public class Stickman
+public class Stickman : MonoBehaviour
 {
-    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
