218e651e3996d17e8d04cef0bc93ec610258b516
Roberto Lucas <lucascastilloroberto@gmail.com>
2026-04-05 17:53:52 +0200

    Stickman script updated for positions

--- Assets/Script/Stickman.cs
+++ Assets/Script/Stickman.cs
@@ -15,10 +15,19 @@ public class Stickman : MonoBehaviour
     private LineRenderer lineRenderer;
     private SpriteRenderer spriteRenderer;
 
+    [Header ("Anchor")]
+    [SerializeField] private GameObject anchor;
+
+    [Header ("variable Private")]
+    private int lastBestPosJoint;
+    private int lastBestPosSelected;
+
     private void Start () {
         hJoint = GetComponent<HingeJoint2D>();
         rb = GetComponent<Rigidbody2D>();
         lineRenderer = GetComponent<LineRenderer>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+
+        anchor.transform.GetChild(lastBestPosSelected).gameObject.GetComponent<Joint>().Selected();
     }
 }
