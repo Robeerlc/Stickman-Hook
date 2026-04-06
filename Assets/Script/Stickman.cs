7c31dca4400e63f95667001b15f95c850b060ee0
SergiuAng <sergiu.angheluta@educa.madrid.org>
2026-04-06 17:47:08 +0200

    script updateç

--- Assets/Script/Stickman.cs
+++ Assets/Script/Stickman.cs
@@ -2,32 +2,130 @@ using UnityEngine;
 
 public class Stickman : MonoBehaviour
 {
-    [Header ("Sprites Player")]
+    [Header("Sprites Player")]
     [SerializeField] Sprite ballSprite;
     [SerializeField] Sprite stopSprite;
     [SerializeField] Sprite goSprite;
     [SerializeField] Sprite backSprite;
     [SerializeField] Sprite winSprite;
 
-    [Header ("Componentes")]
+    [Header("Componentes")]
     private HingeJoint2D hJoint;
     private Rigidbody2D rb;
     private LineRenderer lineRenderer;
     private SpriteRenderer spriteRenderer;
 
-    [Header ("Anchor")]
+    [Header("Anchor")]
     [SerializeField] private GameObject anchor;
 
-    [Header ("variable Private")]
+    [Header("variable Private")]
     private int lastBestPosJoint;
     private int lastBestPosSelected;
+    private int touches;
+    private int bestPos;
+    private float bestDistance;
+    private Vector3 actualJointPos;
 
-    private void Start () {
+    [Header("Variable Public")]
+    [SerializeField] private float gravityRope = 2f;
+    [SerializeField] private float gravityAir = 0.5f;
+    [SerializeField] private float factorX = 1.2f;
+    [SerializeField] private float factorY = 1f;
+
+    [Header("Bool")]
+    private bool sticked = false;
+    private void Start()
+    {
         hJoint = GetComponent<HingeJoint2D>();
         rb = GetComponent<Rigidbody2D>();
         lineRenderer = GetComponent<LineRenderer>();
         spriteRenderer = GetComponent<SpriteRenderer>();
 
+        lastBestPosJoint = 0;
+        lastBestPosSelected = 0;
+        touches = 0;
         anchor.transform.GetChild(lastBestPosSelected).gameObject.GetComponent<Joint>().Selected();
+    }
+
+    private void update()
+    {
+        bestPos = 0;
+        bestDistance = float.MaxValue;
+
+        for (int i = 0; i < anchor.transform.childCount; i++)
+        {
+            {
+                float actualDistance = Vector2.Distance(gameObject.transform.position, anchor.transform.GetChild(i).transform.position);
+                if (actualDistance < bestDistance)
+                {
+                    bestPos = i;
+                    bestDistance = actualDistance;
+                }
+            }
+            CheckInput();
+
+            if (sticked)
+            {
+                lineRenderer.SetPosition(0, gameObject.transform.position);
+                lineRenderer.SetPosition(1, actualJointPos);
+
+                ChangeSprite();
+            }
+
+            if (lastBestPosJoint != bestPos)
+            {
+                anchor.transform.GetChild(lastBestPosSelected).gameObject.GetComponent<Joint>().Unselected();
+                anchor.transform.GetChild(bestPos).gameObject.GetComponent<Joint>().Selected();
+
+                lastBestPosSelected = bestPos;
+            }
+        }
+    }
+
+    private void CheckInput()
+    {
+        if (Input.GetMouseButtonDown(0) || CheckInput.GetKeyDown(KeyCode.Space) || ((Input.touchCount > 0) && (touches == 0)))
+        {
+            lineRenderer.enabled = true;
+            hJoint.enabled = true;
+            rb.gravityScale = gravityRope;
+
+            hJoint.connectedBody = anchor.transform.GetChild(bestPos).transform.GetChild(0).gameObject.GetComponent<Rigidbody2D>();
+            actualJointPos = anchor.transform.GetChild(bestPos).gameObject.transform.position;
+            anchor.transform.GetChild(bestPos).gameObject.GetComponent<Joint>().SetSticked();
+            anchor.transform.GetChild(bestPos).gameObject.GetComponent<Joint>().Unselected();
+
+            lastBestPosJoint = bestPos;
+            rb.angularVelocity = 0f;
+
+            sticked = !sticked;
+
+        }
+
+        if (Input.GetMouseButtonUp(0) || CheckInput.GetKeyUp(KeyCode.Space) || ((Input.touchCount == 0) && (touches > 0)))
+        {
+            lineRenderer.enabled = false;
+            hJoint.enabled = false;
+            rb.linearVelocity = Vector2(rb.linearVelocity.x * factorX, rb.linearVelocity.y + factorY);
+            rb.gravityScale = gravityAir;
+
+            anchor.transform.GetChild(lastBestPosJoint).gameObject.GetComponent<Joint>().SetUnsticked();
+
+            if (bestPos == lastBestPosJoint)
+            {
+                anchor.transform.GetChild(bestPos).gameObject.GetComponent<Joint>().Selected();
+                anchor.transform.GetChild(lastBestPosSelected).gameObject.GetComponent<Joint>().Unselected();
+            }
+
+            spriteRenderer.sprite = ballSprite;
+            rb.AddTorque(-rb.linearVelocity.magnitude);
+            sticked = !sticked;
+        }
+        touches = Input.touchCount;
+    }
+
+    private void ChangeSprite()
+    {
+
     }
 }
