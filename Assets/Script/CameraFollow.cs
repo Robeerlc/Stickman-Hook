e029a01cf7095d405ee96308829e6122e70c3e42
SergiuAng <sergiu.angheluta@educa.madrid.org>
2026-04-06 19:04:23 +0200

    Camara update and player update

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -2,15 +2,45 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject player;
+    [SerializeField] private float speed;
+    private Rigidbody2D playerRb;
+     public float offset;
+     public float maxOfset;
+     public float minX , maxX;
+
+    private void Start()
     {
-        
+        offset = 0;
+        playerRb = player.GetComponent<Rigidbody2D>();
+        gameObject.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, gameObject.transform.position.z); 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (playerRb.linearVelocity.x > 0)
+        {
+            offset += speed * Time.deltaTime;
+            if(offset > maxOfset)
+            {
+                offset = maxOfset;
+            }
+        }
+        else if (playerRb.linearVelocity.x < 0)
+         {
+            offset -= speed * Time.deltaTime;
+            if(offset < -maxOfset)
+            {
+                offset = -maxOfset;
+            }
+         }
+
+         float nextX = player.transform.position.x + offset;
+         if (nextX < minX) nextX = minX;
+         if (nextX > maxX) nextX = maxX;
+
+        gameObject.transform.position = new Vector3(nextX, player.transform.position.y, gameObject.transform.position.z);
+         
     }
+
 }
