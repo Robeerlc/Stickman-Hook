0553ba28af828355ea9244c4f975480f368d296d
Sanvo <marco.sanvoisin@educa.madrid.org>
2026-04-23 21:29:25 +0200

    Added GameManager Script and FinishLine Sprite

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Transform finishLine;
+    [SerializeField] private CameraFollow cameraFollow;
+    [SerializeField] private Stickman stickman;
+}
