15298c108fc1e2edc10574960c36292609bb17a5
Sanvo <marco.sanvoisin@educa.madrid.org>
2026-04-23 21:32:34 +0200

    Created Start() and Update() methods on GameManager.cs

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -5,4 +5,16 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Transform finishLine;
     [SerializeField] private CameraFollow cameraFollow;
     [SerializeField] private Stickman stickman;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject particleEffect;
+
+    private void Start(){
+        stickman = player.GetComponent<Stickman>();
+        initPos = player.transform.position;
+    }
+
+    private void Update(){
+        
+    }
+
 }
