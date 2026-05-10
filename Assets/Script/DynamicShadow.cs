e303d16f94834c60472af410fc17c4a794354230
Jose <43698177+joseluisgp@users.noreply.github.com>
2026-05-10 19:43:39 +0200

    Reformatted code
    Signed-off-by: Jose <43698177+joseluisgp@users.noreply.github.com>

--- Assets/Script/DynamicShadow.cs
+++ Assets/Script/DynamicShadow.cs
@@ -10,19 +10,19 @@ public class DynamicShadow : Shadow
     public float maxOffset;
     [SerializeField] private CameraFollow cameraFollow;
 
-    private void LateUpdate ()
+    private void LateUpdate()
     {
-        if(isDynamic)
+        if (isDynamic)
         {
             shadowSpriteRenderer.sprite = spriteRenderer.sprite;
             shadowSpriteRenderer.flipX = spriteRenderer.flipX;
             float offset = 0.1f;
-            if(cameraFollow.maxOffset != 0)
+            if (cameraFollow.maxOffset != 0)
             {
                 offset = (cameraFollow.offset / cameraFollow.maxOffset) * (maxOffset * 100);
                 offset /= 100;
             }
-            shadowGameObject.transform.position = gameObject.transform.position + new Vector3 (shadowOffset.x, shadowOffset.y) + new Vector3 (-offset, 0f, 0f);
+            shadowGameObject.transform.position = gameObject.transform.position + new Vector3(shadowOffset.x, shadowOffset.y) + new Vector3(-offset, 0f, 0f);
         }
     }
 }
--- Assets/Script/JointAnchor.cs
+++ Assets/Script/JointAnchor.cs
@@ -15,47 +15,56 @@ public class JointAnchor : MonoBehaviour
     [SerializeField] private float animTime = 0.1f;
     [SerializeField] public AnimationCurve animationCurve;
 
-    void Start () {
-        spriteRenderer = GetComponent<SpriteRenderer> ();
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         defaultSprite = spriteRenderer.sprite;
 
-        if (spriteUnsticked == null) {
+        if (spriteUnsticked == null)
+        {
             spriteUnsticked = defaultSprite;
         }
 
-        if (spriteSticked == null) {
+        if (spriteSticked == null)
+        {
             spriteSticked = defaultSprite;
         }
 
-        dashLine = gameObject.transform.GetChild (1).gameObject;
+        dashLine = gameObject.transform.GetChild(1).gameObject;
     }
 
-    public void SetSticked () {
+    public void SetSticked()
+    {
         spriteRenderer.sprite = spriteSticked;
         sticked = true;
     }
 
-    public void SetUnsticked () {
+    public void SetUnsticked()
+    {
         spriteRenderer.sprite = spriteUnsticked;
         sticked = false;
-        
+
     }
 
-    public void Selected () {
+    public void Selected()
+    {
         if (!sticked) StartCoroutine(SelectingJoint());
         else dashLine.transform.localScale = Vector3.zero;
     }
 
-    public void Unselected () {
+    public void Unselected()
+    {
         StartCoroutine(SelectingJoint());
         dashLine.transform.localScale = Vector3.zero;
     }
 
-    IEnumerator SelectingJoint () {
+    IEnumerator SelectingJoint()
+    {
         float time = 0f;
         Vector3 startScale = Vector3.zero;
-        Vector3 endScale = new Vector3 (1.13f, 1.13f, 1.13f);
-        while (time <= animTime) {
+        Vector3 endScale = new Vector3(1.13f, 1.13f, 1.13f);
+        while (time <= animTime)
+        {
             time += Time.deltaTime;
             dashLine.transform.localScale = Vector3.Lerp(startScale, endScale, animationCurve.Evaluate(time));
             yield return null;
